352c8925ccff697fda29070e78c06e783854c0da
steveski <steve@steve-york.com>
2022-09-23 23:12:08 +1000

    feat: added TextChangedEventArgsConverter

---
+++ src/CommunityToolkit.Maui.UnitTests/Converters/TextChangedEventArgsConverterTests.cs
@@ -0,0 +1,51 @@
+﻿using System.Globalization;
+using CommunityToolkit.Maui.Converters;
+using CommunityToolkit.Maui.UnitTests.Extensions;
+using Xunit;
+
+namespace CommunityToolkit.Maui.UnitTests.Converters;
+
+public class TextChangedEventArgsConverterTests : BaseOneWayConverterTest<TextChangedEventArgsConverter>
+{
+	public static IReadOnlyList<object?[]> Data { get; } = new[]
+	{
+		new object?[] { null, null},
+		new object?[] { new TextChangedEventArgs("Snoogan", "Snoogans"), "Snoogans"},
+	};
+
+	[Theory]
+	[MemberData(nameof(Data))]
+	public void TextChangedEventArgsConverter(TextChangedEventArgs value, object? expectedResult)
+	{
+		var textChangedEventArgsConverter = new TextChangedEventArgsConverter();
+
+		var convertResult = ((ICommunityToolkitValueConverter)textChangedEventArgsConverter).Convert(value, typeof(object), null, CultureInfo.CurrentCulture);
+		var convertFromResult = textChangedEventArgsConverter.ConvertFrom(value);
+
+		Assert.Equal(expectedResult, convertResult);
+		Assert.Equal(expectedResult, convertFromResult);
+	}
+
+	[Theory]
+	[InlineData(1)]
+	[InlineData(1.1)]
+	[InlineData('c')]
+	[InlineData(true)]
+	public void InvalidConverterValuesThrowsArgumentException(object value)
+	{
+		var textChangedEventArgsConverter = new TextChangedEventArgsConverter();
+		Assert.Throws<ArgumentException>(() => ((ICommunityToolkitValueConverter)textChangedEventArgsConverter).Convert(value, typeof(object), null, CultureInfo.CurrentCulture));
+	}
+
+	[Fact]
+	public void TextChangedEventArgsConverterNullInputTest()
+	{
+#pragma warning disable CS8625 // Cannot convert null literal to non-nullable reference type.
+		Assert.Throws<ArgumentNullException>(() => ((ICommunityToolkitValueConverter)new TextChangedEventArgsConverter()).Convert(new TextChangedEventArgs("", ""), null, null, null));
+		Assert.Throws<ArgumentNullException>(() => ((ICommunityToolkitValueConverter)new TextChangedEventArgsConverter()).Convert(new TextChangedEventArgs(null, ""), null, null, null));
+		Assert.Throws<ArgumentNullException>(() => ((ICommunityToolkitValueConverter)new TextChangedEventArgsConverter()).Convert(new TextChangedEventArgs("", null), null, null, null));
+		Assert.Throws<ArgumentNullException>(() => ((ICommunityToolkitValueConverter)new TextChangedEventArgsConverter()).ConvertBack("", null, null, null));
+		Assert.Throws<ArgumentNullException>(() => ((ICommunityToolkitValueConverter)new TextChangedEventArgsConverter()).ConvertBack(null, null, null, null));
+#pragma warning restore CS8625 // Cannot convert null literal to non-nullable reference type.
+	}
+}
---
+++ src/CommunityToolkit.Maui/Converters/TextChangedEventArgsConverter.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+
+namespace CommunityToolkit.Maui.Converters;
+
+/// <summary>
+/// Converts/Extracts the incoming value from <see cref="TextChangedEventArgs"/> object and returns the value of <see cref="TextChangedEventArgs.NewTextValue"/> property from it.
+/// </summary>
+public class TextChangedEventArgsConverter : BaseConverterOneWay<TextChangedEventArgs?, object?>
+{
+    /// <inheritdoc/>
+    public override object? DefaultConvertReturnValue { get; set; } = null;
+
+    /// <summary>
+    /// Converts/Extracts the incoming value from <see cref="TextChangedEventArgs"/> object and returns the value of <see cref="TextChangedEventArgs.NewTextValue"/> property from it.
+    /// </summary>
+    /// <param name="value">The value to convert.</param>
+    /// <param name="culture">(Not Used)</param>
+    /// <returns>A <see cref="TextChangedEventArgs.NewTextValue"/> object from object of type <see cref="TextChangedEventArgs"/>.</returns>
+    [return: NotNullIfNotNull("value")]
+    public override object? ConvertFrom(TextChangedEventArgs? value, CultureInfo? culture = default) => value switch
+    {
+        null => null,
+        _ => value.NewTextValue
+    };
+
+}
